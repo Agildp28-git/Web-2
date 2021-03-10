e06fdf44e2b1a1a791a82e716f632fa5530224c6
Agildp28 <agildogil28@gmail.com>
2021-03-10 23:39:13 +0700

    bismillah6

---
+++ minggu2/if_else2.php
@@ -0,0 +1,9 @@
+<?php
+$user 	= "Agildp28";
+$pass 	= "123";
+if ($user == "Agildp28" && $pass == "123") {
+		echo "Login Berhasil";
+} else {
+		echo "Login gagal";
+}
+?>
--- minggu2/variabel.php
+++
@@ -1,6 +0,0 @@
-<?php
-$nim = "A11.2020.02824";
-$nama = 'Agil Dwi Putra';
-echo "NIM : " . $nim . "<br>";
-echo "Nama : $nama";
-?>
