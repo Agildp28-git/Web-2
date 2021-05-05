ced99371c7a56b1b61d6181651aa4fe8ee0efac7
Agildp28 <agildogil28@gmail.com>
2021-05-05 13:40:07 +0700

    Bismillah

---
+++ minggu8/session01.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+<title>Login here...</title>
+</head>
+<body>
+<form action="" method="post">
+<h2>Login Here...</h2>
+<label>Username : <label><br>
+	<input type="text"name="user"><br>
+<label>Password : </label><br>
+<input type="password"name="pass"><br>
+<input type="submit" name="Login" value="Log In">
+</form></body></html>
+<html>
+<?php
+session_start();
+if (isset ($_POST['Login'])) {
+$user = $_POST['user'];
+$pass = $_POST['pass'];
+//periksa login
+if ($user == "achmatim" && $pass = "123") {
+//menciptakan session
+$_SESSION['login'] = $user;
+//menuju ke halaman pemeriksaan session
+echo "<h1>Anda berhasil LOGIN</h1>";
+echo "<h2>Klik <a href='session02.php'>di sini
+(session02.php)</a>
+untuk menuju ke halaman pemeriksaan session";
+}
+} else {
+}
+?>
